26c49d5092f3ff34003711b9d6f0029c806c5a68
Maticruces <104586656+Maticruces@users.noreply.github.com>
2023-06-10 19:47:48 -0400

    agregacion de clases y funciones
    agregacion de pseudo codigos y ejemplo de main del mini proyecto 2

---
+++ QuadTree.h
@@ -0,0 +1,18 @@
+#ifdef QUADTREE_H
+#define QUADTREE_H
+
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+
+int totalPoints(); 
+int totalNodes(); 
+void insert(Point p, int data); 
+list(); 
+int countRegion(Point p, int d); 
+int AggregateRegion(Point p, int d); 
+
+/* Nota: Cualquier función adicional que sea implementada optará a puntaje adicional. La cantidad de puntaje extra queda a criterio del profesor. */
+#endif
