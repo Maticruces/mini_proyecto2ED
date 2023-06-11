e850188bfa2f7197143a46b1e9eb308a7ab7eee3
aguicam1 <136203680+aguicam1@users.noreply.github.com>
2023-06-10 20:18:27 -0400

    Update QuadTree.h
    Agregué la implementación propuesta de GeeksforGeeks, pero agregando quadrants que es la lista de 4 enlaces a otros nodos y un string para guardar si es blanco o negro

--- QuadTree.h
+++ QuadTree.h
@@ -1,18 +1,61 @@
 #ifdef QUADTREE_H
 #define QUADTREE_H
-
 #include <vector>
-#include <iostream>
+#include <string>
 
 using namespace std;
 
+struct Point{
+  int x, y;
+  Point(int _x, int _y){
+    x = _x;
+    y = _y;
+  }
+  Point(){
+    x = 0;
+    y = 0;
+  }
+};
 
-int totalPoints(); 
-int totalNodes(); 
-void insert(Point p, int data); 
-list(); 
-int countRegion(Point p, int d); 
-int AggregateRegion(Point p, int d); 
+struct Node{
+  Node* quadrants[4];
+  Point pos;
+  string nodeType;
+  int data;
+  Node(Point p, string nType, int d){
+    pos = p;
+    nodeType = nType;
+    data = d;
+    for(int i = 0; i < 4; i++){
+      quadrants[i] = nullptr;
+    }
+  }
+  Node(){
+    for(int i = 0; i < 4; i++){
+      quadrants[i] = nullptr;
+    }
+  }
+};
+
+class QuadTree{
+  private:
+    Point topLeft;
+    Point bottomRight;
+    Node* n;
+    QuadTree* topLeftTree;
+    QuadTree* topRightTree;
+    QuadTree* bottomLeftTree;
+    QuadTree* bottomRightTree;
+    int pointCount;
+  public:
+    QuadTree(Point topLeft, Point bottomRight);
+    int totalPoints(); 
+    int totalNodes(); 
+    void insert(Point p, int data); 
+    vector<Node> list(); 
+    int countRegion(Point p, int d); 
+    int AggregateRegion(Point p, int d); 
+};
 
 /* Nota: Cualquier función adicional que sea implementada optará a puntaje adicional. La cantidad de puntaje extra queda a criterio del profesor. */
 #endif
