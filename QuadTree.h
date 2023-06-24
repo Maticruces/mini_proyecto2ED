1b733ea04eda0d24819ee187f3b46c798202946b
aguicam1 <136203680+aguicam1@users.noreply.github.com>
2023-06-24 15:25:11 -0400

    Update QuadTree.h

--- QuadTree.h
+++ QuadTree.h
@@ -1,67 +1,82 @@
-#ifndef QUADTREE_H
-#define QUADTREE_H
-#include <vector>
 #include <string>
-#define nullptr NULL
-
+#include <vector>
 using namespace std;
 
+/**
+ * @brief 'Point' define una estructura de datos que representa un punto en un sistema de coordenadas 2D.
+ * 
+ * Contiene dos variables de tipo double, 'x' e 'y', que representan las coordenadas del punto en el plano.
+ * 
+ * Tiene además, dos constructores: uno que toma como parámetro dos valores 'double' e inicializa 'x' e 'y'
+ * con estos valores. Y otro sin parámetros que inicializa ambas variables en 0.0.
+ */
 struct Point{
-  double x, y;
-  string nombreciudad;  //x:horizontal:longitud   &   y:vertical:latitud ... En coordenadas va (y,x) Latitud,Longitud "a LA.LO le gusta la geografia"
-  long pobla;
-  Point(double _x, double _y, string nm, long po){  //caso con parametros
-      x = _x;
-      y = _y;
-      nombreciudad = nm;
-      pobla = po;
-      
-  }
-  Point(){  //en caso de no poner parametros
-      x = 0.0;
-      y = 0.0;
-      nombreciudad = "NO_GESTIONADO";
-      pobla = 0;
-  }
-};  
+    double x, y;
+    Point(double _x, double _y){
+        x = _x;
+        y = _y;
+    }
+    Point(){
+        x = 0.0;
+        y = 0.0;
+    }
+};
 
+/**
+ * @brief ' Node' define una estructura de datos de tipo nodo para la clase QuadTree.
+ *
+ * Contiene un punto 'pos' que representa la posición del nodo, una variable string 'nodeType' que representa
+ * el tipo de nodo ("white" o "black"), un vector<int> 'data' que almacena información asociada al nodo 
+ * (población para esta implementación) y un vector<Node*> 'quadrants' que almacena 4 enlaces a otros nodos 
+ * representando cuadrantes del plano.
+ * 
+ * Tiene 2 constuctores: uno que toma un punto como parámetro y asigna la posición del nodo, además de 
+ * inicializar el resto de variables. Y otro constructor sin parámetros que inicializa las variables con 
+ * sus valores por defecto.
+ */
 struct Node{
-  Point pos;
-  string nodeType;
-  vector<int> data;
-  vector<Node*> quadrants;
-  Node(Point _pos){
-      pos = _pos;
-      data.clear();
-      quadrants.resize(4, nullptr);
-  }
+    Point pos;
+    string nodeType;
+    vector<int> data;
+    vector<Node*> quadrants;
+    Node(Point _pos){
+        pos = _pos;
+        nodeType = "";
+        data.clear();
+        quadrants.resize(4, nullptr);
+    }
+    Node(){
+        pos.x = 0.0;
+        pos.y = 0.0;
+        nodeType = "";
+        data.clear();
+        quadrants.resize(4, nullptr);
+    }
+
 };
 
+/**
+ * Definición de la clase 'QuadTree' que representa una estructura de datos quadtree.
+ */
 class QuadTree{
-  private:
-    Point topLeft;
-    Point bottomRight;
-    Node* n;
-    QuadTree* topLeftTree;
-    QuadTree* topRightTree;
-    QuadTree* bottomLeftTree;
-    QuadTree* bottomRightTree;
-    int pointCount;
-    bool inBoundary(Point p);
-  public:
-    QuadTree();
-    QuadTree(Point topLeft, Point bottomRight);
-    int totalPoints();
-    int totalNodes();
-    void insert(Node* node);
-    void insert(Point p, int data);//auxiliar para el otro insert
-    vector<Node> list(); 
-    int countRegion(Point p, int d); 
-    int AggregateRegion(Point p, int d); 
+    private:
+        Point topLeft;
+        Point bottomRight;
+        Node* n;
+        QuadTree* topLeftTree;
+        QuadTree* topRightTree;
+        QuadTree* bottomLeftTree;
+        QuadTree* bottomRightTree;
+        int pointCount;
+        bool inBoundary(Point p);
+    public:
+        QuadTree();
+        QuadTree(Point topLeft, Point bottomRight);
+        int totalPoints();
+        int totalNodes();
+        void insert(Node* node);
+        void insert(Point p, int data);
+        vector<Node> list();
+        int countRegion(Point p, int d);
+        int aggregateRegion(Point p, int d);
 };
-
-
-#endif
-
-/* Nota: Cualquier función adicional que sea implementada optará a puntaje adicional. La cantidad de puntaje extra queda a criterio del profesor. */
-
